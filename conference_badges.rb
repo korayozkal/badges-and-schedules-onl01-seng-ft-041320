10b2edfa9ba8789f36e9a1cf47ce744515471bd6
Koray Ozkal <koryozkal@gmail.com>
2020-04-18 22:41:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(attendees)
+  badge_list = []	  
+  attendees.collect { |badges| badge_maker(badges) }
+end 
+
+def assign_rooms(attendees)
+  room_assignments=[]
+  attendees.collect { |rooms|  badge_maker(rooms)}
+return Hello, #{attendees} You'll be assigned to room #{rooms}!"
+end
+
